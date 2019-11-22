4153724411be58873158b94e1b26cfadc52e940f
unknown <onucsecu@gmail.com>
2019-11-22 20:33:54 +0600

    google image crawler version 1.0

--- src/GPhotoDownloader.java
+++ src/GPhotoDownloader.java
@@ -29,6 +29,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.SwingWorker;
 
@@ -40,6 +42,8 @@ public class GPhotoDownloader extends JFrame {
 	private JLabel 		label;
 	private JProgressBar 	bar;
 	private int n;
+
+
 	public String report="Number of Images Processed :";
 	public int err=0;
 	private static String DIR =System.getProperty("user.home")+"\\Google";
@@ -110,10 +114,10 @@ public class GPhotoDownloader extends JFrame {
 		        String baseUrl = UrltextField.getText();	
 		        n=Integer.valueOf(numtextField.getText());
 		        
-		        bar.setMinimum(0);
-		        bar.setMaximum(n);
+		        bar.setDoubleBuffered(true);
+		        bar.setIndeterminate(true);
 		        SaveImage simage=new SaveImage(baseUrl,n,label,bar);
-				simage.addPropertyChangeListener(new ProgressListener(bar));
+				simage.addPropertyChangeListener(new ProgressListener(bar,label));
 				simage.execute();
 				 System.out.println("Finished!!!");
 			}
@@ -134,7 +138,7 @@ public class GPhotoDownloader extends JFrame {
 		contentPane.add(bar);
 		
 		label = new JLabel("--");
-		label.setBounds(10, 236, 124, 14);
+		label.setBounds(10, 236, 345, 14);
 		contentPane.add(label);
 	}
 }
@@ -145,6 +149,9 @@ class SaveImage extends SwingWorker<Integer, Integer>{
 	private int n;
 	private JLabel label;
 	public boolean err=false;
+//	public static List<Long> urls = new ArrayList<>();
+//	public static List<String> base_urls = new ArrayList<>();
+	
 	public SaveImage(String url1,int n1,JLabel lb,JProgressBar b) {
 		// TODO Auto-generated constructor stub
 		baseUrl=url1;
@@ -154,62 +161,165 @@ class SaveImage extends SwingWorker<Integer, Integer>{
 	}
 
 	@Override
-	protected  Integer doInBackground() throws Exception {
+	protected  Integer doInBackground() {
 		// TODO Auto-generated method stub
+			
 			int progress=0;
-			
-			WebDriver driver =new ChromeDriver() ;
-		 	driver.get(baseUrl);
-	        try{
-	        	WebElement element =driver.findElement(By.xpath("//*[@id=\"rg_s\"]/div[1]"));
-	        	
-	        	element.click();
-	        }catch (Exception ex) {
-	        	err=true;
-	        	 System.out.println("prblm ");
-	        	 label.setText("Error!! plz try again ");
-	        	 bar.setForeground(Color.RED);
-	        	 bar.setIndeterminate(true);
-	        	 bar.setStringPainted(false);
-	        	 driver.close();
-	        }
-			for(int i=2,j=1,k=1;k<=n;j++,i++) {
-				if(i==4) {
-					i=1;
-				}
-				WebElement element1 =driver.findElement(By.xpath("//*[@id=\"irc-ss\"]/div["+i+"]/div[1]/div[2]"));
-				element1.click();
-				WebElement element2 =driver.findElement(By.xpath("//*[@id=\"irc-ss\"]/div[2]/div[1]/div[4]/div[1]/a/div/img"));
-				String url = element2.getAttribute("src");
-				if(j!=1 && j%3!=2 )
-					continue;
-				System.out.println(url);
+			List<Long> urls = new ArrayList<>();
+			List<String> base_urls = new ArrayList<>();
+			List<String> img_urls = new ArrayList<>();
+			base_urls.add(baseUrl);
+			int k=0;
+			while(!base_urls.isEmpty()) {
 				k++;
-		 		progress=k;
-		 		setProgress(progress);
-		 		publish(progress);
-				SaveImageW simage=new SaveImageW(url,k);
-				simage.start();
-		        synchronized(simage){
-		            try{
-		                System.out.println("Waiting for the image to save...");
-		                simage.wait();
-		            }catch(Exception es){
-		                es.printStackTrace();
-		            }
-		 
-		            System.out.println("track another image ");
+				WebDriver driver =new ChromeDriver() ;
+				String first = base_urls.get(0);
+			 	driver.get(first);
+			 	base_urls.remove(0);
+				WebElement element =null;
+				element =driver.findElement(By.xpath("//*[@id=\"rg_s\"]/div[1]"));
+				if(!element.isDisplayed()) {
+					element =driver.findElement(By.xpath("//*[@id=\"islrg\"]/div[1]/div[1]/a[1]/div[1]"));
+				}
+				
+				
+		        try{
+		        	element.click();
+		        }catch (Exception ex) {
+		        	err=true;
+		        	 System.out.println("prblm ");
+		        	 label.setText("Error!! plz try again ");
+		        	 bar.setForeground(Color.RED);
+		        	 bar.setIndeterminate(true);
+		        	 bar.setStringPainted(false);
+		        	 driver.close();
 		        }
-			
+		        
+				int cnt =0;
+				int i=2;
+				boolean flag=true;
+				String past_url = null;
+				for(;progress<=n;progress++) {	
+					WebElement ele=null;
+					try {
+						ele =driver.findElement(By.cssSelector("#irc-ss > div:nth-child("+(i+1)+") > div.irc_t.i30052 > div.Q8Bv4e.irc-rac"));
+					}catch (Exception e) {
+						// TODO: handle exception
+					}
+					
+					int c=0;
+					while(flag==true && ele.isDisplayed()==false && c<4) {
+							i++;
+							c++;
+							if(i == 4) {
+								i=1;
+							}
+							ele =driver.findElement(By.cssSelector("#irc-ss > div:nth-child("+(i+1)+") > div.irc_t.i30052 > div.Q8Bv4e.irc-rac"));
+							System.out.println(i+" "+ele.isDisplayed());
+					}
+					
+					if(ele.isDisplayed()==false) {
+						flag=false;
+					}
+					
+					WebElement element1 =null;
+					WebElement element2 =null;
+					WebElement element3 = null;
+					try {
+						if(flag) 
+							element2=driver.findElement(By.xpath("//*[@id=\"irc-ss\"]/div["+i+"]/div[1]/div[4]/div[1]/a/div/img"));
+						else
+							element2=driver.findElement(By.xpath("//*[@id=\"Sva75c\"]/div/div/div[3]/div[2]/div/div[1]/div[1]/div/div[2]/a/img"));
+						if(flag)
+							element1 = driver.findElement(By.xpath("//*[@id=\"irc-ss\"]/div["+i+"]/div[1]/div[2]"));
+						else
+							element1 = driver.findElement(By.xpath("//*[@id=\"Sva75c\"]/div/div/div[3]/div[2]/div/div[1]/div[1]/div/div[1]/a[2]/div"));
+						if(flag)
+							element3= driver.findElement(By.xpath("//*[@id=\"irc-ss\"]/div["+i+"]/div[2]/div[2]/div/div[1]/a"));
+						else
+							element3= driver.findElement(By.xpath("//*[@id=\"Sva75c\"]/div/div/div[3]/div[2]/div/div[1]/div[3]/div[3]/c-wiz/div/div/div/div[1]/a"));
+					}catch (Exception e) {
+						// TODO: handle exception
+					}
+					try {
+						String side_url= element3.getAttribute("href");
+						base_urls.add(side_url);//store images similar google photos search link
+						
+					}catch (Exception e) {
+						// TODO: handle exception
+					}
+					String url = element2.getAttribute("src");
+					
+					if(past_url!=null && past_url.equals(url)) {
+						cnt++;
+						if(cnt>3) {
+							break;
+						}
+						element1.click();
+						continue;
+					}
+					past_url=url;
+					long hc=url.hashCode();   //check if the image is from same location
+					if(urls.contains(hc)) {
+						element1.click();
+						continue;
+					}
+					urls.add(hc);				// if no add to the list and download the image
+					System.out.println(progress);
+					System.out.println(url);
+			 	
+			 		setProgress(progress*100/n);
+			 		publish(progress);
+			 		
+//					SaveImageW simage=new SaveImageW(url,progress);
+//					simage.start();
+//			        synchronized(simage){
+//			            try{
+//			                System.out.println("Waiting for the image to save...");
+//			                simage.wait();
+//			                
+//			                
+//			            }catch(Exception es){
+//			                es.printStackTrace();
+//			                System.out.print(es.getMessage());
+//			            }
+//			          
+//			        }
+			 		img_urls.add(url);
+			 		past_url=url;
+			 		element1.click();
+				}
+				driver.close();
+				if(progress>=n) {
+					break;
+				}
 			}
-			driver.close();
 			if(err) {
 				label.setText("Error!! plz try again ");
 			}else {
-				label.setText("Alhamdulillah!");
+				label.setText("Alhamdulillah!Now Initiating Download");
+				bar.setBackground(Color.orange);
+				bar.setIndeterminate(true);
 			}
-			System.out.println("Finished!!!");
-		
+			System.out.println("******Crawling Ended****!!!");
+	        for (int i=0; i<img_urls.size(); i++) { 
+	           String url=img_urls.get(i);
+				SaveImageW simage=new SaveImageW(url,i+1);
+				simage.start();
+		        synchronized(simage){
+		            try{
+		                System.out.println(i+1+" Waiting for the image to save.");
+		                simage.wait();
+		            }catch(Exception es){
+		                es.printStackTrace();
+		                System.out.print(es.getMessage());
+		            }
+		          
+		        }
+		        progress=i;
+		 		setProgress(progress*100/n);
+		 		publish(progress);
+	        }
 		return progress;
 	}
 
@@ -230,6 +340,7 @@ class SaveImageW extends Thread {
     public void run(){
     	boolean er=false;
         synchronized(this){
+
    		 String toFile =System.getProperty("user.home")+"\\Google\\"+String.valueOf(i)+".jpg" ;
    		 URL website = null;
 		try {
@@ -270,7 +381,6 @@ class SaveImageW extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			er=true;
-
 		}
    		 notify();
         }
@@ -278,16 +388,19 @@ class SaveImageW extends Thread {
 }
 class ProgressListener implements PropertyChangeListener {
 	private JProgressBar bar;
-	ProgressListener(JProgressBar b) {
+	private JLabel label;
+	ProgressListener(JProgressBar b,JLabel lb) {
 		this.bar = b;
-		bar.setValue(0);
+		this.label=lb;
 	}
 
 	public void propertyChange(PropertyChangeEvent evt) {
 		//Determine whether the property is progress type
 		System.out.println(evt.getPropertyName());
 		if ("progress".equals(evt.getPropertyName())) { 
+			bar.setIndeterminate(false);
 			bar.setValue((int) evt.getNewValue());
+			label.setText("images are being collected...");
 		}
 	}
 }
